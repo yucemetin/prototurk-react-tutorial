35f5fe47370a1020fca7cdb777b9bf5f779f5eca
yucemetin <red.banana988@gmail.com>
2022-09-07 00:09:10 +0300

    environment variables are stored

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./App.css"
 function App() {
   const className = process.env.NODE_ENV === "production" ? "info production" : "info development"
   return (
+    <>
+      <h1>{process.env.REACT_APP_API_URL}</h1>
+      <div className={className}>
+        <h3>{process.env.NODE_ENV}</h3>
+        <img src="/logo512.png" alt="" />
+      </div>
+    </>
 
-    <div className={className}>
-      <h3>{process.env.NODE_ENV}</h3>
-      <img src="/logo512.png" alt="" />
-    </div>
 
   );
 }
